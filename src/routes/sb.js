6f5b0bdccc7fd8c4da043d6f09ebce2a98ebb777
Tomohiro SHIOYA <shioyang4321@gmail.com>
2017-01-08 15:15:55 +0900

    test API to retrieve video details

--- src/routes/sb.js
+++ src/routes/sb.js
@@ -4,11 +4,11 @@ var router = express.Router();
 var YouTube = require('youtube-node');
 var youTube = new YouTube();
 
-var youTubeAPIKey = process.env.YOUTUBE_API_KEY;
+var youTubeAPIKey = process.env.YOUTUBE_APIKEY;
 youTube.setKey(youTubeAPIKey);
 
 /*** Utils ***/
-var logError = function(err) { console.log(err); }
+var logError = function(err){ console.log(err); }
 
 /*** REST ***/
 /* GET */
@@ -16,9 +16,24 @@ var logError = function(err) { console.log(err); }
  * Response
  *   API list
  */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next){
   res.render('api_list', { title: 'SB API List' });
   //TODO
 });
+
+router.get('/test', function(req, res, next){
+  youTube.getById('0E00Zuayv9Q', function(error, result){
+    // https://www.youtube.com/watch?v=0E00Zuayv9Q
+    if(error){
+      logError(error);
+      res.send(error);
+    }else{
+      res.send(result);
+    }
+  });
+});
+
+/*** POST ***/
+
 
 module.exports = router;
