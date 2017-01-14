7bf34be7cd71f8cdd92bfc121d12305483a5514f
Tomohiro SHIOYA <shioyang4321@gmail.com>
2017-01-14 23:30:54 +0900

    Show bar chart

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, Params } from '@angular/router';
 import 'rxjs/add/operator/switchMap';
-import * as d3 from 'd3-selection';
+// import * as d3 from 'd3-selection';
+import * as d3 from 'd3';
 
 import { SbService } from '../sb.service';
 import { Video } from '../video';
@@ -24,7 +25,7 @@ export class PlaylistComponent implements OnInit {
       .switchMap((params: Params) => this.sbService.getPlaylistItemsDetails(params['playlistId']))
       .subscribe((videos: Video[]) => {
         this.initSvg();
-        this.showVideos(videos);
+        this.showVideos(videos, 'viewCount');
       });
   }
 
@@ -41,13 +42,32 @@ export class PlaylistComponent implements OnInit {
                         .attr('height', 400);
   }
 
-  showVideos(videos: Video[]): void {
-    this.svg.selectAll('text').data(videos).enter()
-      .append('text')
-        .attr('font-size', '12px')
-        .attr('fill', 'black')
-        .attr('y', (d, i) => (14 * (i + 1)))
-        .text(d => d.snippet.title + ' view:' + d.statistics.viewCount);
+  showVideos(videos: Video[], stat: string): void {
+    let x = d3.scaleBand()
+              .range([0, 400])
+              .padding(0.2);
+    let y = d3.scaleLinear()
+              .range([400, 0]);
+
+    x.domain(videos.map(d => d.snippet.title));
+    y.domain([0, d3.max(videos, d => d.statistics.viewCount - 0)]);
+
+    let sss = d3.max(videos, d => d.statistics.viewCount);
+
+    this.svg.selectAll('.bar').data(videos)
+        .enter().append('rect')
+          .attr('class', 'bar')
+          .attr('x', d => x(d.snippet.title))
+          .attr('width', x.bandwidth())
+          .attr('y', d => y(d.statistics.viewCount))
+          .attr('height', d => 400 - y(d.statistics.viewCount));
+
+    // this.svg.selectAll('text').data(videos).enter()
+    //   .append('text')
+    //     .attr('font-size', '12px')
+    //     .attr('fill', 'black')
+    //     .attr('y', (d, i) => (14 * (i + 1)))
+    //     .text(d => d.snippet.title + ' view:' + d.statistics.viewCount);
   }
 
 }
