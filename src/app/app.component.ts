965f65ba5808ba03f4474791f05058b02d9940dd
Tomohiro SHIOYA <shioyang4321@gmail.com>
2017-01-14 16:03:23 +0900

    Show playlist links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PLAYLIST_DATA } from './playlist';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'app works!';
+  playlist_data: { name: string, playlistId: string }[] = PLAYLIST_DATA;
 
   constructor() { }
 
