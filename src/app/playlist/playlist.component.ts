2fadc2ba9a495f6a95559098bea699e08611e2e3
Tomohiro SHIOYA <shioyang4321@gmail.com>
2017-01-15 23:42:32 +0900

    Change axis and show tick

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -38,37 +38,41 @@ export class PlaylistComponent implements OnInit {
     }
     this.svg = svgArea.append('svg')
                         .classed('SbSvg', true)
-                        .attr('width', 400 + 40)
+                        .attr('width', 400 + 40 + 400)
                         .attr('height', 400 + 40)
                       .append('g')
                         .attr('transform', 'translate(' + 20 + ',' + 20 + ')');
   }
 
   showVideos(videos: Video[], stat: string): void {
-    let x = d3.scaleBand()
+    let x = d3.scaleLinear()
+              .range([400, 0]);
+    let y = d3.scaleBand()
               .range([0, 400])
               .padding(0.2);
-    let y = d3.scaleLinear()
-              .range([400, 0]);
 
-    x.domain(videos.map(d => d.snippet.title));
-    y.domain([0, d3.max(videos, d => d.statistics.viewCount - 0)]);
+    x.domain([0, d3.max(videos, d => d.statistics.viewCount - 0)]);
+    y.domain(videos.map(d => d.snippet.title));
 
     let sss = d3.max(videos, d => d.statistics.viewCount);
 
     this.svg.selectAll('.bar').data(videos)
         .enter().append('rect')
           .attr('class', 'bar')
-          .attr('x', d => x(d.snippet.title))
-          .attr('width', x.bandwidth())
-          .attr('y', d => y(d.statistics.viewCount))
-          .attr('height', d => 400 - y(d.statistics.viewCount));
+          .attr('x', d => x(d.statistics.viewCount))
+          .attr('width', d => (400 + 20) - x(d.statistics.viewCount))
+          .attr('y', d => y(d.snippet.title))
+          .attr('height', y.bandwidth())
+          .attr('fill', 'skyblue')
+          .attr('opacity', .8);
 
     this.svg.append('g')
-            .attr('transform', 'translate(0, ' + 400 + ')')
+            .attr('transform', 'translate(20, ' + 400 + ')')
             .call(d3.axisBottom(x));
     this.svg.append('g')
-            .call(d3.axisLeft(y));
+            .attr('transform', 'translate(' + 420 + ', 0)')
+            .call(d3.axisRight(y));
+                    // .tickFormat( d3.format('.1') ));
 
     // this.svg.selectAll('text').data(videos).enter()
     //   .append('text')
