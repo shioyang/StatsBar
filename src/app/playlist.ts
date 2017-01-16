7323fbf0c25c350718efeae90cbaa42b0a339470
Tomohiro SHIOYA <shioyang4321@gmail.com>
2017-01-16 21:33:14 +0900

    Revert "Fix nav link loop"
    This reverts commit 8a52053c124937ed1728364736bfe5aa481496e2.

--- src/app/playlist.ts
+++
@@ -1,5 +0,0 @@
-export const PLAYLIST_DATA: { name: string, playlistId: string }[] = [
-  // { name: 'MV: IKIMONOGAKARI', playlistId: 'PLJTTYqJcK6QJmukXM52MTHQGPANlLmB4b' },
-  // { name: 'MV: SEKAI NO OWARI', playlistId: 'PL5K5CcQ_R3OSuV4PDwS5YS1Yg06cbtuBl' },
-  { name: 'MV: JJ', playlistId: 'PLs8AlpdTjgwdSDETD55q0i3W98tC9SAur' }
-];
